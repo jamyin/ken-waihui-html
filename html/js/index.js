9fc6bd83f8a88dad28b3539e036d2d437af7a352
openks <openks@foxmail.com>
2016-03-04 13:45:44 +0800

    导航栏三种状态 Signed-off-by: openks <openks@foxmail.com>

--- html/js/index.js
+++ html/js/index.js
@@ -16,42 +16,5 @@ $(function() {
 		var src = $img.attr("src");
 		$img.attr("src", src.replace("-1", "-0"));
 	});
-	
-
-	$(".aboutus").hover(function(){
-		$(this).find("a").removeClass("aboutus-a").text("公司介绍");
-	},function(){
-		$(this).find("a").text("").addClass("aboutus-a");
-	});
-	$(".qual").hover(function(){
-		$(this).find("a").removeClass("qual-a").text("公司资质");
-	},function(){
-		$(this).find("a").text("").addClass("qual-a");
-	});
-	$(".catering").hover(function(){
-		$(this).find("a").removeClass("catering-a").text("宴会类型");
-	},function(){
-		$(this).find("a").text("").addClass("catering-a");
-	});
-	$(".menu").hover(function(){
-		$(this).find("a").removeClass("menu-a").text("美食菜单");
-	},function(){
-		$(this).find("a").text("").addClass("menu-a");
-	});
-	$(".activity").hover(function(){
-		$(this).find("a").removeClass("activity-a").text("活动案例");
-	},function(){
-		$(this).find("a").text("").addClass("activity-a");
-	});
-	$(".field").hover(function(){
-		$(this).find("a").removeClass("field-a").text("场地推荐");
-	},function(){
-		$(this).find("a").text("").addClass("field-a");
-	});
-	$(".contanct").hover(function(){
-		$(this).find("a").removeClass("contanct-a").text("联系我们");
-	},function(){
-		$(this).find("a").text("").addClass("contanct-a");
-	});
 
 });
