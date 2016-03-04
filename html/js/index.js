ca28de9a4499d7945171b50f8855c59c27227a04
openks <openks@foxmail.com>
2016-03-04 14:23:09 +0800

    菜单价格设置为可编辑 Signed-off-by: openks <openks@foxmail.com>

--- html/js/index.js
+++ html/js/index.js
@@ -6,15 +6,21 @@ $(function() {
 		var src = $(this).find("img").attr("src");
 		$(this).find("img").attr("src", src.replace("1", "0"));
 	});
-	
+
 	$(".pri").hover(function() {
 		var $img = $(this).find("img");
-		var src = $img.attr("src");
-		$img.attr("src", src.replace("-0", "-1"));
+		var src = "";
+		if ($(this).hasClass("p6")) {
+			src = "img/S5100.png";
+		} else if ($(this).hasClass("p7")) {
+			src = "img/S5200.png";
+		} else if ($(this).hasClass("p8")) {
+			src = "img/S5300.png";
+		}
+		$img.attr("src", src);
 	}, function() {
 		var $img = $(this).find("img");
-		var src = $img.attr("src");
-		$img.attr("src", src.replace("-1", "-0"));
+		$img.attr("src", "img/S5000.png");
 	});
 
 });
